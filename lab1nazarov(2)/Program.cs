56e7a307c9c738984b7fae4f1fe5c68f1cbde8ab
Ruslan Nazarov <rnazarov142@gmail.com>
2023-09-15 20:50:59 +0200

    Add project files.

---
+++ lab1nazarov(2)/Program.cs
@@ -0,0 +1,11 @@
+﻿//Высокий уровень (2-ое задание)
+double C = Math.Pow(10, -6);
+double L = 0.04;
+int U = 100;
+
+double I = U * Math.Sqrt(C / L);
+Console.WriteLine($"I = {I}A");
+Console.WriteLine("Далее найдем W - энергию");
+
+double W = (L * (I * I)) / 2;
+Console.WriteLine($"И так, W в данной задаче равно {W:F2}J");
---
+++ lab1nazarov/Program.cs
@@ -0,0 +1,12 @@
+﻿//Высокий уровень
+Console.Write("Введите x: ");
+double x = double.Parse(Console.ReadLine());
+Console.Write("Введите b: ");
+double b = double.Parse(Console.ReadLine());
+Console.Write("Введите a: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Введите y: ");
+double y = double.Parse(Console.ReadLine());
+
+double K = (Math.Sqrt(x + b - a) + Math.Log(y)) / (Math.Atan(b + a));
+Console.WriteLine($"K = {K:F2}");
